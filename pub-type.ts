20c19c6797721cee05d9bd16a7bb3e46c22efdb8
Super-Smile <85211574+Super-Smile@users.noreply.github.com>
2021-10-14 21:19:27 +0900

    unique void
    unique void update

--- pub-type.ts
+++ pub-type.ts
@@ -5,17 +5,10 @@ export interface Metric {
   // The current value of the metric.
   value: number;
 
-  // The delta between the current value and the last-reported value.
-  // On the first report, `delta` and `value` will always be the same.
+ 
   delta: number;
 
-  // A unique ID representing this particular metric instance. This ID can
-  // be used by an analytics tool to dedupe multiple values sent for the same
-  // metric instance, or to group multiple deltas together and calculate a
-  // total. It can also be used to differentiate multiple different metric
-  // instances sent from the same page, which can happen if the page is
-  // restored from the back/forward cache (in that case new metrics object
-  // get created).
+  
   id: string;
 
   // Any performance entries used in the metric value calculation.
